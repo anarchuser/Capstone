27949750dcd06838c0191f262590fcb8328787d4
Aaron Alef <aaron.alef@code.berlin>
2022-04-27 17:31:35 +0200

    test(world): ensure physical world starts empty without gravity

--- test/src/World/World.cpp
+++ test/src/World/World.cpp
@@ -1,2 +1,24 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "Game/World/World.h"
+
+#define WINDOW_SIZE 100
+#define PHYS_SCALE  0.1
+
+SCENARIO ("World handles objects as expected") {
+    GIVEN ("An empty world") {
+        Stage::instance = new Stage;
+        getStage()->setSize (WINDOW_SIZE, WINDOW_SIZE);
+
+        auto world = kt::World (nullptr, {WINDOW_SIZE * PHYS_SCALE, WINDOW_SIZE * PHYS_SCALE});
+
+        THEN ("The physical world starts empty") {
+            auto & physical_world = world.world;
+
+            REQUIRE (physical_world.GetBodyCount() == 0);
+            REQUIRE (physical_world.GetGravity() == b2Vec2_zero);
+        }
+    }
+}
 
 /* Copyright © 2022 Aaron Alef */
--- test/src/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-
-TEST_CASE ("Tests be like") {
-    CHECK (true);
-}
-
-/* Copyright (C) 2022 Aaron Alef */
