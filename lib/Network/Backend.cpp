46ed9cbb00b0a94da263116685d82804e00be69e
Aaron Alef <aaron.alef@code.berlin>
2022-06-02 16:59:32 +0200

    refactor(synchro): relay registrar construction
    NOTE: This automatically means all other Backend methods are unused as
    well, and therefore get removed

--- lib/Network/Backend.cpp
+++ lib/Network/Backend.cpp
@@ -39,7 +39,7 @@ namespace cg {
 
         auto results = context.getResults();
         results.setId (ID);
-        results.setRegistrar (newRegistrar (remoteID));
+        results.setRegistrar (synchro.newRegistrar (remoteID));
         results.setSynchro (synchro.newSynchro (remoteID));
 
         return kj::READY_NOW;
@@ -58,133 +58,6 @@ namespace cg {
 
         /// Connect back to received synchro
         return synchro.connectTo (remoteID, params.getRemote());
-    }
-
-    ::kj::Own <RegistrarImpl> BackendImpl::newRegistrar (ClientID const & id) {
-        auto registrar = kj::heap <RegistrarImpl> (id);
-        registrar->setOnRegisterShip (LAMBDA (registerShip));
-        return registrar;
-    }
-
-    ::kj::Own <ShipSinkImpl> BackendImpl::registerShip (Spaceship const & ship, ClientID const & id, ShipHandle_t handle) {
-        auto * client = findClient (id);
-        KJ_REQUIRE_NONNULL (client, id, "Ship owner is not registered");
-        auto & ships = client->ships;
-        auto username = ship.username;
-        KJ_REQUIRE (!ships.contains (username), id, username, "Client 'id' already contains a ship named 'username'");
-
-        log ("Registering ship " + username + " from client " + id);
-        log ("Position: / "  + std::to_string (ship.position[0]) + " | " + std::to_string (ship.position[1]) + " \\");
-        log ("Velocity: \\ " + std::to_string (ship.velocity[0]) + " | " + std::to_string (ship.velocity[1]) + " /");
-        log ("Health: " + std::to_string (ship.health));
-
-        ships.emplace (username, handle);
-        broadcastSpaceship (ship, handle, remotes.contains (id));
-
-        // Prepare ShipSink
-        auto sink = kj::heap <ShipSinkImpl>();
-        sink->setOnDone ([this, username] () {
-            return doneCallback (username);
-        });
-        sink->setOnSendItem ([this, id] (Direction const & direction, Spaceship const & data) {
-            return sendItemCallback (direction, data, id);
-        });
-        return sink;
-    }
-
-    void BackendImpl::broadcastSpaceship (Spaceship const & ship, ShipHandle_t handle, bool isRemote) {
-        if (local.has_value()) {
-            log ("Broadcast ship " + ship.username + " to local client");
-            detach (distributeSpaceship (ship, handle, local.value()));
-        }
-        for (auto & client : remotes) {
-            if (isRemote) continue;
-
-            log ("Broadcast ship " + ship.username + " to remote client " + client.first);
-            detach (distributeSpaceship (ship, handle, client.second));
-        }
-    }
-    kj::Promise <void> BackendImpl::distributeSpaceship (Spaceship const & ship, ShipHandle_t handle, Client & receiver) {
-        auto & username = ship.username;
-        if (receiver.sinks.contains (username)) return kj::READY_NOW;
-
-        auto request = receiver.registrar.registerShipRequest ();
-        ship.initialise (request.initShip ());
-        request.setHandle (handle);
-        return request.send().then ([this, username, & receiver]
-                (capnp::Response<Backend::Registrar::RegisterShipResults> results) {
-                    // Check again if nothing's changed
-                    if (receiver.sinks.contains (username)) return;
-                    receiver.sinks.insert_or_assign (username, results.getSink ());
-                });
-    }
-    void BackendImpl::doneCallback (ShipName const & username) {
-        log ("Ship " + username + " is done");
-        for (auto & client : remotes) {
-            detach (client.second.erase (username)
-                    .then ([this, id = client.first] () {disconnect (id);}));
-        }
-    }
-    void BackendImpl::sendItemCallback (Direction const & direction, Spaceship const & data, ClientID const & id) {
-        auto * sender = findClient (id);
-        auto & username = data.username;
-        KJ_REQUIRE_NONNULL (sender, id, username, "Received item from unregistered client");
-        if (sender->ships.empty()) {
-            disconnect (id);
-            return;
-        }
-        KJ_ASSERT (sender->ships.contains (username), id, username, "Client does not own ship 'username'");
-        auto & handle = sender->ships.at (username);
-
-        // If item came from local client -> distribute it to all remote clients
-        if (id == ID) {
-            for (auto & client : remotes) {
-                sendItemToClient (direction, data, handle, client.second).detach (
-                        [this, id = client.first] (kj::Exception && e) { disconnect (id); });
-            }
-        }
-        // Distribute item to all local clients
-        if (local.has_value()) {
-            detach (sendItemToClient (direction, data, handle, local.value()));
-        }
-    }
-    kj::Promise <void> BackendImpl::sendItemToClient (Direction const & direction, Spaceship const & data, ShipHandle_t handle, Client & receiver) {
-        auto & sinks = receiver.sinks;
-        auto & username = data.username;
-
-        if (!sinks.contains (username)) {
-            return handle.getShipRequest().send()
-                    .then ([this, handle, & receiver] (auto results) mutable {
-                        return distributeSpaceship (Spaceship (results.getShip()), handle, receiver);
-                    });
-        }
-        auto request = sinks.at (username).sendItemRequest();
-        direction.initialise (request.initItem().initDirection());
-        data.initialise (request.getItem().initSpaceship());
-        return request.send().ignoreResult()
-                .catch_ ([this, & sinks, & username] (kj::Exception && e) { sinks.erase (username); });
-    }
-
-    kj::Promise <void> BackendImpl::disconnect (ClientID const & id) {
-        log ("Disconnecting client " + id);
-        auto * client = findClient (id);
-        if (!client) return kj::READY_NOW;
-
-        // TODO: tie each promise in the loop to the returned promise
-        for (auto const & sink : client->sinks) {
-            detach (client->erase (sink.first));
-        }
-        remotes.erase (id);
-    }
-
-    Client * BackendImpl::findClient (ClientID const & id) {
-        if (remotes.contains (id)) return & remotes.at (id);
-        if (id == ID) {
-            if (local.has_value()) return & local.value();
-
-            KJ_LOG (WARNING, "Local client requested but it does not exist (yet)");
-        }
-        return nullptr;
     }
 }
 
--- lib/Network/Backend.h
+++ lib/Network/Backend.h
@@ -34,33 +34,6 @@ namespace cg {
         std::optional <LocalClient> local;                   /// The one allowed *local* client connected
         std::unordered_map <ClientID, RemoteClient> remotes; /// List of all *remote* clients connected
         SynchroImpl synchro;
-        // TODO: do we need a whole map for local clients?
-
-        /// Return raw pointer to Client if id was in remote or local. Nullptr otherwise
-        Client * findClient (ClientID const & id);
-
-        /* Ship registration functions */
-        /// Configure the newly registered spaceship and return a sink to it. Registrar callback
-        kj::Own <ShipSinkImpl> registerShip    (Spaceship const & ship, ClientID const & id, ShipHandle_t);
-        /// Distribute spaceship to every client
-        void broadcastSpaceship  (Spaceship const & ship, ShipHandle_t handle, bool isRemote);
-        /// Hand out spaceship to the specified client
-        kj::Promise <void> distributeSpaceship (Spaceship const & ship, ShipHandle_t handle, Client & receiver);
-
-        /* ShipSink callback functions */
-        /// Callback on closing sink; propagate to sink of all clients
-        void doneCallback     (ShipName const & username);
-        /// Callback on item received; propagate to sink of all clients
-        void sendItemCallback (Direction const & direction, Spaceship const & data, ClientID const & id);
-        /// Helper to propagate item to sink of one specific client
-        kj::Promise <void> sendItemToClient (Direction const & direction, Spaceship const & data, ShipHandle_t handle, Client & receiver);
-
-        /* Impl builder functions */
-        /// Build a new Registrar with registerShip as callback
-        kj::Own <RegistrarImpl> newRegistrar (ClientID const & id);
-
-        /// Disconnect the client with this id, if exists
-        kj::Promise <void> disconnect (ClientID const & id);
 
         /// Log function of this implementation
         void log (std::string const & msg);
