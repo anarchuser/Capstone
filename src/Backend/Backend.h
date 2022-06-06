be65819876b738939436be0714ce624523b94e88
Aaron Alef <aaron.alef@code.berlin>
2022-06-06 17:27:37 +0200

    docs(backend): clean up docs of the backend wrapper

--- src/Backend/Backend.h
+++ src/Backend/Backend.h
@@ -19,25 +19,35 @@
 namespace kt {
     using namespace oxygine;
 
+    /// Wrapper around the Cap'n Proto server instance acting as actual backend
     class Backend {
     private:
-        std::size_t seed;
+        /// Seed of this game's instance
+        std::size_t const seed;
 
+        /// Thread the actual server is running on
         std::thread server_thread;
+        /// Address the server is bound to
         std::string const address;
+        /// Port the server is listening to
         std::atomic <short> port = -1;
 
+        /// Flag to tell the server to stop
         std::atomic <bool> stop = false;
 
+        /// Start the server on the server_thread
         void serve ();
 
     public:
+        /// Start a new server
         Backend (std::size_t seed, std::string address);
-        ~Backend () noexcept;
+        /// Tell the server thread to stop and wait until it did so
+        ~Backend ();
 
-        [[nodiscard]] unsigned short getPort() const;
-        [[nodiscard]] std::string const & getAddress() const;
+        /// Wait for the server port to resolve to a positive value indicating the server is up and running
+        unsigned short getPort() const;
 
+        /// Send a ping to the given address and return success status
         static bool ping (std::string const & ip, short port);
     };
 
